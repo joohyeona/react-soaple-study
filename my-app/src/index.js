a9bce2f2f437f571d62e184131f61c311e97644d
이주현 <joohyeon505@gmail.com>
2025-07-20 18:24:26 +0900

    state, 생명주기 mount unmount 관련 alert 띄우기

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -7,12 +7,13 @@ import reportWebVitals from './reportWebVitals';
 import Library from './book/Library';
 import Clock from './clock/Clock';
 import CommentList from './comments/CommentList';
+import NotificationList from './alert/NotificationList';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <Library />
+    <NotificationList />
     <CommentList />
   </React.StrictMode>
 );
